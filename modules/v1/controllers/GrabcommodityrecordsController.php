93f5a2dde8190af7e881d2d9fac97803826e3e2d
jiaweizhou <jiaweizhou@zju.edu.cn>
2015-11-29 14:58:35 +0800

    dddd

--- modules/v1/controllers/GrabcommodityrecordsController.php
+++ modules/v1/controllers/GrabcommodityrecordsController.php
@@ -65,7 +65,7 @@ class GrabcommodityrecordsController extends Controller
 	{
 		$data=Yii::$app->request->post();
 		$query = (new \yii\db\Query ())
-		->select('grabcommodityrecords.id as flag, grabcommodityrecords.*,grabcommodities.picture,grabcommodities.title,grabcommodities.version,grabcommodities.date,grabcommodities.number,grabcommodities.end_at,grabcommodities.islotteried,users.nickname,users.thumb')
+		->select('grabcommodityrecords.id as flag, grabcommodityrecords.*,grabcommodities.picture,grabcommodities.title,grabcommodities.version,grabcommodities.date,grabcommodities.winnernumber,grabcommodities.end_at,grabcommodities.islotteried,users.nickname,users.thumb')
 		->from('grabcommodityrecords')
 		->orderBy('grabcommodityrecords.created_at desc')
 		->join('INNER JOIN','grabcommodities','grabcommodityrecords.grabcommodityid = grabcommodities.id and winnerrecordid = grabcommodityrecords.id')
