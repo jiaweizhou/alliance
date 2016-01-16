5e8bb5a1dd1804b1b63a6cb773e4a62baad1447c
jiaweizhou <jiaweizhou@zju.edu.cn>
2016-01-16 14:08:28 +0800

    lalala

--- modules/v1/controllers/RecommendationsController.php
+++ modules/v1/controllers/RecommendationsController.php
@@ -108,7 +108,37 @@ class RecommendationsController extends Controller
 		//$dataProvider->
 		return $result;
 	}
-
+	function getDistance($lat1, $lng1, $lat2, $lng2)
+	{
+		$earthRadius = 6367000; //approximate radius of earth in meters
+	
+		/*
+		 Convert these degrees to radians
+		 to work with the formula
+		 */
+	
+		$lat1 = ($lat1 * pi() ) / 180;
+		$lng1 = ($lng1 * pi() ) / 180;
+	
+		$lat2 = ($lat2 * pi() ) / 180;
+		$lng2 = ($lng2 * pi() ) / 180;
+	
+		/*
+		 Using the
+		 Haversine formula
+	
+		 http://en.wikipedia.org/wiki/Haversine_formula
+	
+		 calculate the distance
+		 */
+	
+		$calcLongitude = $lng2 - $lng1;
+		$calcLatitude = $lat2 - $lat1;
+		$stepOne = pow(sin($calcLatitude / 2), 2) + cos($lat1) * cos($lat2) * pow(sin($calcLongitude / 2), 2);  $stepTwo = 2 * asin(min(1, sqrt($stepOne)));
+		$calculatedDistance = $earthRadius * $stepTwo;
+	
+		return round($calculatedDistance);
+	}
     /**
      * Creates a new Applyjobs model.
      * If creation is successful, the browser will be redirected to the 'view' page.
--- modules/v1/controllers/TbothersController.php
+++ modules/v1/controllers/TbothersController.php
@@ -35,7 +35,56 @@ class TbothersController extends Controller {
 		$dataProvider=new ActiveDataProvider([
 				'query' => $query,
 		]);
+		
+		$daters = $dataProvider->getModels();
+		//$result = array ();
+		//$result ['item'] = array ();
+		
+		//$tbreplys = (new \yii\db\Query ())->select('tbreplys.*,users.phone,users.nickname,users.thumb')->orderBy ( "tbreplys.created_at desc" )->join ( 'INNER JOIN', 'users', ' tbmessages.userid =users.id ')->where('tbreplys.messageid in ');
+		
+		foreach ( $daters as $i=>$dater ) {
+			$info = $dater;
+				
+			$info["distance"] = $this->getDistance($latitude, $longitude, $info['latitude'], $info['longitude']);
+		
+			$daters[$i] = $info;
+		
+		}
+		$dataProvider->setModels($daters);
+		
 		return $dataProvider;
+	}
+	
+	function getDistance($lat1, $lng1, $lat2, $lng2)
+	{
+		$earthRadius = 6367000; //approximate radius of earth in meters
+	
+		/*
+		 Convert these degrees to radians
+		 to work with the formula
+		 */
+	
+		$lat1 = ($lat1 * pi() ) / 180;
+		$lng1 = ($lng1 * pi() ) / 180;
+	
+		$lat2 = ($lat2 * pi() ) / 180;
+		$lng2 = ($lng2 * pi() ) / 180;
+	
+		/*
+		 Using the
+		 Haversine formula
+	
+		 http://en.wikipedia.org/wiki/Haversine_formula
+	
+		 calculate the distance
+		 */
+	
+		$calcLongitude = $lng2 - $lng1;
+		$calcLatitude = $lat2 - $lat1;
+		$stepOne = pow(sin($calcLatitude / 2), 2) + cos($lat1) * cos($lat2) * pow(sin($calcLongitude / 2), 2);  $stepTwo = 2 * asin(min(1, sqrt($stepOne)));
+		$calculatedDistance = $earthRadius * $stepTwo;
+	
+		return round($calculatedDistance);
 	}
 	
 	public function actionMy(){
